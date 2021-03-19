de9af15f74827cef1acf969035c5c136545c5bc3
Bosunwill <54271663+Bosunwill@users.noreply.github.com>
2021-03-18 20:13:53 -0500

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Gui Ndikum on 3/18/21.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
